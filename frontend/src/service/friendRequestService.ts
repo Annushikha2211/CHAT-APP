16e50f9b842e602e3f1a2f8cdf2818977c8a8d56
Annushikha <annushikha1508@gmail.com>
2026-08-30 18:35:29 +0530

    fix sendotp problm

--- frontend/src/service/friendRequestService.ts
+++ frontend/src/service/friendRequestService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  `${import.meta.env.VITE_BASE_URL}/friend-requests`;
+  `${import.meta.env.VITE_BASE_URL}/api/friend-requests`;
 
 const getConfig = () => {
   const token = localStorage.getItem("token");
--- frontend/src/service/userService.ts
+++ frontend/src/service/userService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   import.meta.env.VITE_BASE_URL
-    ? `${import.meta.env.VITE_BASE_URL}/users`
+    ? `${import.meta.env.VITE_BASE_URL}/api/users`
     : "http://localhost:5000/api/users";
 
 // ===============================
